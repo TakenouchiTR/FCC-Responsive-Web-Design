3e8e0f08ba15d1b642e81152958d7f435fa06f68
Shawn Carter <totakenouchi@outlook.com>
2020-10-15 20:53:00 -0400

    Fix problem with previous commit

--- front-end/md-viewer/src/index.js
+++ front-end/md-viewer/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import marked from "marked";
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
-
+//Placeholder shamelessly taken from the FCC example page
 const placeholder = `# Welcome to my React Markdown Previewer!
 
 ## This is a sub-heading...
@@ -132,8 +132,3 @@ ReactDOM.render(
   <Wrapper />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
