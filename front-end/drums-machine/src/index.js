6df82bc10ca0b7dee3409b64d9c702e039c5a896
Shawn Carter <totakenouchi@outlook.com>
2020-10-26 09:17:18 -0400

    Update layout of drum machine

--- front-end/drums-machine/src/index.js
+++ front-end/drums-machine/src/index.js
@@ -3,114 +3,103 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
-const quotes = [
-  "This is a quote",
-  "This is also a quote",
-  "This, too, is a quote",
-]
+const PAD_INFO = [
+  {char: "Q", keyNum: 81, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "W", keyNum: 87, url: "https://freesound.org/data/previews/34/34828_209299-lq.mp3", name: "Snare"}, 
+  {char: "E", keyNum: 69, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "A", keyNum: 65, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "S", keyNum: 83, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "D", keyNum: 68, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "Z", keyNum: 90, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "X", keyNum: 88, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}, 
+  {char: "C", keyNum: 67, url: "https://freesound.org/data/previews/0/428_196-lq.mp3", name: "Kick"}
+];
 
-const authors = [
-  "Shawn",
-  "Also Shawn",
-  "Still Shawn"
-]
-
-const generateQuote = () => {
-  let ind = Math.floor(Math.random() * quotes.length);
-  return {
-    quote: quotes[ind],
-    author: authors[ind]
-  };
-}
-
-class Quote extends React.Component {
+class DrumButton extends React.Component {
   constructor(props) {
     super(props);
+
+    this.button_click = this.button_click.bind(this);
+    this.key_press = this.key_press.bind(this);
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.key_press);
+  }
+
+  button_click() {
+    this.playSound()
+  }
+
+  key_press(e) {
+    if (e.keyCode === this.props.keyNum)
+      this.playSound();
+  }
+
+  playSound() {
+    const audio = document.getElementById(this.props.char);
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  render() {
+    const buttonId = "drum-" + this.props.char;
+    return (
+      <button id={buttonId} className="drum-pad" onClick={this.button_click}>
+        {this.props.char}
+        <audio id={this.props.char} className="clip" src={this.props.url}></audio>
+      </button>
+    )
+  }
+}
+
+class Control extends React.Component {
   render() {
     return (
-      <div id="text">
-          <p className="center">{this.props.quote}</p>
+      <div><p>ipsum lorem</p></div>
+    )
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    let buttons = this.props.pads.map(
+      (item, key) => (<DrumButton key={key} char={item.char} keyNum={item.keyNum} url={item.url}/>)
+    );
+
+    return (
+      <div id="display">
+        <div id="button-group">
+          {buttons}
+        </div>
+        <div id="control-group">
+          <Control />
+        </div>
       </div>
     )
   }
 }
 
-class Author extends React.Component {
+class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
-  }
 
+   this.state = {
+
+    };
+  }
+  
   render() {
     return (
-      <div id="author">
-          <p className="center">- {this.props.author}</p>
+      <div id="drum-machine">
+        <Display pads={PAD_INFO}/>
       </div>
-    )
-  }
-}
-
-class QuoteButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div id="btn-box">
-        <button id="new-quote" className="center" onClick={this.props.handler}>New Quote</button>
-      </div>
-    )
-  }
-}
-
-class Social extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div id="social">
-        <p><a id="tweet-quote" className="center" href="twitter.com/intent/tweet">Tweet this quote</a></p>
-      </div>
-    )
-  }
-}
-
-class QuoteBox extends React.Component {
-  constructor(props) {
-    super(props);
-    const newQuote = generateQuote();
-    this.state = {
-      quote: newQuote.quote,
-      author: newQuote.author
-    }
-  }
-
-  quoteButton_click() {
-    const newQuote = generateQuote();
-    this.setState({
-      quote: newQuote.quote,
-      author: newQuote.author
-    })
-  }
-
-  render() {
-    return  (
-      <div id="quote-box">
-        <Quote quote={this.state.quote}/>
-        <Author author={this.state.author}/>
-        <QuoteButton handler={this.quoteButton_click.bind(this)} />
-        <Social />
-      </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(
-  <QuoteBox />,
+  <DrumMachine />,
   document.getElementById('root')
 );
 
